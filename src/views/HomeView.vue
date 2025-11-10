e3b045c256aeb8d2beb03278d35811dfb34ab5d5
dominique.goncalves61 <dominique.goncalves61@gmail.com>
2025-11-10 13:27:29 +0000

    incluído

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="p-8 max-w-4xl mx-auto">
+      <h1 class="text-3xl font-bold mb-4">Template Vue 3 + Tailwind + TypeScript (Full)</h1>
+      <p class="mb-6">Este template já contém lint, prettier, husky, vitest, cypress e CI.</p>
+      <div class="flex gap-3">
+        <router-link to="/game" class="px-4 py-2 bg-cyan-500 rounded">Ir para o jogo</router-link>
+      </div>
+    </div>
+</template>
+  
+<script setup lang="ts"></script>
