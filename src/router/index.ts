9a9b15252de4390644247b07bcbfd47c45414490
dominique.goncalves61 <dominique.goncalves61@gmail.com>
2025-11-10 13:17:14 +0000

    incluído

---
+++ src/router/index.ts
@@ -0,0 +1,13 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const routes = [
+  { path: '/', name: 'home', component: () => import('@/views/HomeView.vue') },
+  { path: '/game', name: 'game', component: () => import('@/views/GameView.vue') },
+]
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+})
+
+export default router
