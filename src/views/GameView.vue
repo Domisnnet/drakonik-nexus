42b3668e3fc49cb95daf0083c58196a938f65b55
dominique.goncalves61 <dominique.goncalves61@gmail.com>
2025-11-11 19:24:16 +0000

    ajustado

--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -1,12 +1,14 @@
 <template>
   <BaseLayout>
-    <div class="relative flex flex-col items-center justify-center w-full h-screen overflow-hidden">
+    <div class="relative flex flex-col items-center justify-center w-full h-full">
+      <FlipCard />
       <SliderNavigator />
     </div>
   </BaseLayout>
 </template>
 
 <script setup>
-import BaseLayout from "@/components/layouts/BaseLayout.vue";
+import FlipCard from "@/components/game/FlipCard.vue";
 import SliderNavigator from "@/components/ui/SliderNavigator.vue";
+import BaseLayout from "@/components/layouts/BaseLayout.vue";
 </script>
