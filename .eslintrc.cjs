031cd360861a301153d47a4d497c700dc80e88cd
dominique.goncalves61 <dominique.goncalves61@gmail.com>
2025-11-14 18:31:30 +0000

    reinstalado

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,20 +5,29 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  parser: 'vue-eslint-parser',
+
+  parser: "vue-eslint-parser",
+
   parserOptions: {
-    parser: '@typescript-eslint/parser',
-    sourceType: 'module',
-    ecmaVersion: 'latest',
+    parser: "@typescript-eslint/parser",
+    sourceType: "module",
+    ecmaVersion: "latest",
+    extraFileExtensions: [".vue"],
   },
+
   extends: [
-    'eslint:recommended',
-    'plugin:vue/vue3-recommended',
-    'plugin:@typescript-eslint/recommended'
+    "eslint:recommended",
+    "plugin:vue/vue3-essential", 
+    "plugin:@typescript-eslint/recommended",
   ],
-  plugins: [
-    'vue',
-    '@typescript-eslint'
-  ],
-  rules: {}
-}
+
+  rules: {
+    "vue/html-self-closing": "off",
+    "vue/singleline-html-element-content-newline": "off",
+    "vue/multiline-html-element-content-newline": "off",
+    "vue/max-attributes-per-line": "off",
+
+    "@typescript-eslint/no-explicit-any": "off",
+    "vue/multi-word-component-names": "off",
+  },
+};
